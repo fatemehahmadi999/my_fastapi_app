1bc512328fd6894f82797eba3f1321dee30dcb6b
Ammar Saqib <ammarsaqib123@gamil.com>
2026-05-13 21:49:28 +0500

    added get and post method for root address

--- app/asgi.py
+++ app/asgi.py
@@ -1,7 +1,16 @@
 import fastapi
 import uvicorn
+from fastapi.responses import HTMLResponse
 
 app = fastapi.FastAPI()
+
+@app.post("/")
+def read_root():
+    return HTMLResponse("<h1> Post Method: Hello Miss Fatemah!</h1>")
+
+@app.get("/")
+def read_root():
+    return HTMLResponse("<h1> Get Method: Hello Miss Fatemah!</h1>")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
